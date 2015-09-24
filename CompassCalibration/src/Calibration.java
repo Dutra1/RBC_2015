35fa8e7ab269573d741969fc041638cb7b141bf8
Dutra <andresduarte92@gmail.com>
2015-09-23 23:57:55 -0300

    Agregado proyectos de testeo

---
+++ CompassCalibration/src/Calibration.java
@@ -0,0 +1,62 @@
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+import lejos.nxt.MotorPort;
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.TachoMotorPort;
+import lejos.nxt.addon.CompassHTSensor;
+import lejos.nxt.addon.LnrActrFirgelliNXT;
+import lejos.util.Delay;
+
+public class Calibration {
+
+	public static final NXTRegulatedMotor leftMotor =  Motor.A;
+	public static final NXTRegulatedMotor rightMotor =  Motor.B;
+	public static final TachoMotorPort mastMotor = MotorPort.C;
+	
+	public static final SensorPort compassPort =  SensorPort.S3;
+	
+	public static final int motorSpeed = 75;
+	public static final int delay = 75 * 1000;
+	 
+	public static void main(String argv[]) {
+		CompassHTSensor compass = new CompassHTSensor(compassPort);
+		LnrActrFirgelliNXT mast = new LnrActrFirgelliNXT(mastMotor);
+		
+		//Start
+		LCD.drawString("Compass calibration", 0, 0);
+		LCD.drawString("Click any button to start", 0, 1);
+		Button.waitForAnyPress();
+		
+		LCD.clear();
+		LCD.drawString("Don't touch while it's calibrating", 0, 0);
+		
+		//Raise mast
+		mast.setPower(100);
+		mast.moveTo(200, false);
+		
+		//Rotate Slowly
+		leftMotor.setSpeed(motorSpeed);
+		rightMotor.setSpeed(motorSpeed);
+		
+		leftMotor.forward();
+		rightMotor.backward();
+		
+		//Calibrate
+		compass.startCalibration();
+		Delay.msDelay(delay);
+		compass.stopCalibration();
+		
+		//Stop
+		leftMotor.stop();
+		rightMotor.stop();
+		
+		//LowerMast
+		LCD.drawString("Click left or right to lower mast", 0, 0);
+		int button = Button.waitForAnyPress();
+		if ((button == Button.ID_LEFT) || (button == Button.ID_RIGHT)) {
+			mast.moveTo(0, false);
+		}
+	}
+}
---
+++ CompassTesting/src/Testing.java
@@ -0,0 +1,70 @@
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+import lejos.nxt.MotorPort;
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.Sound;
+import lejos.nxt.TachoMotorPort;
+import lejos.nxt.addon.CompassHTSensor;
+import lejos.nxt.addon.LnrActrFirgelliNXT;
+import lejos.util.Delay;
+
+public class Testing {
+
+	public static final NXTRegulatedMotor leftMotor =  Motor.A;
+	public static final NXTRegulatedMotor rightMotor =  Motor.B;
+	public static final TachoMotorPort mastMotor = MotorPort.C;
+	
+	public static final SensorPort compassPort =  SensorPort.S3;
+	
+	public static final int motorSpeed = 100;
+	public static final int delay = 500;
+	
+	public static void main(String argv[]) {
+		CompassHTSensor compass = new CompassHTSensor(compassPort);
+		LnrActrFirgelliNXT mast = new LnrActrFirgelliNXT(mastMotor);
+		
+		//Start
+		LCD.drawString("Compass testing", 0, 1);
+		LCD.drawString("Click any button to start", 0, 0);
+		Button.waitForAnyPress();
+		
+		//Raise mast
+		mast.setPower(100);
+		mast.moveTo(200, false);
+		
+		//Rotate Slowly
+		leftMotor.setSpeed(motorSpeed);
+		rightMotor.setSpeed(motorSpeed);
+		
+		leftMotor.forward();
+		rightMotor.backward();
+		
+		//Test
+		compass.resetCartesianZero();
+		LCD.clear();
+		LCD.drawString("Click any button to stop", 0, 0);
+		
+		boolean stop = false;
+		while(!stop) {
+			LCD.clear(1);
+			LCD.drawString("Angle: " + compass.getDegreesCartesian(), 0, 1);
+			Sound.playTone((int) Math.abs(compass.getDegreesCartesian()) + 100, delay);
+			Delay.msDelay(delay);
+			
+			if (Button.readButtons() != 0) stop = true;
+		}
+		
+		//Stop
+		leftMotor.stop();
+		rightMotor.stop();
+		
+		//LowerMast
+		LCD.drawString("Click left or right to lower mast", 0, 0);
+		int button = Button.waitForAnyPress();
+		if ((button == Button.ID_LEFT) || (button == Button.ID_RIGHT)) {
+			mast.moveTo(0, false);
+		}
+	}
+}
